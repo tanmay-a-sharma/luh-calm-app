6d75c030972e5e6392ea546347d55567ea9bf7f6
tanmay-a-sharma <40327232+tanmay-a-sharma@users.noreply.github.com>
2023-12-25 16:47:24 -0800

    updated the login screen with respective buttons and design

--- components/Login/LoginScreen.js
+++ components/Login/LoginScreen.js
@@ -1,12 +1,87 @@
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View
+} from 'react-native';
 
+function LoginScreen({ navigation }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-function LoginScreen(){
-      return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>Login Screen</Text>
-        </View>
-      );
+  // Function to handle the login logic will go here
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.loginContainer}>
+        <Text style={styles.loginTitle}>Login to Your Account</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          placeholderTextColor="#c0af46"
+          value={username}
+          onChangeText={setUsername}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          placeholderTextColor="#c0af46"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+        <TouchableOpacity
+          style={styles.loginButton}
+          onPress={() => {/* Function to handle login */}}
+        >
+          <Text style={styles.loginButtonText}>LOGIN</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#093671',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  loginContainer: {
+    width: '80%',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 8,
+  },
+  loginTitle: {
+    fontSize: 22,
+    color: '#093671',
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    width: '100%',
+    backgroundColor: '#ecf0f1',
+    padding: 10,
+    borderRadius: 5,
+    marginVertical: 10,
+  },
+  loginButton: {
+    backgroundColor: '#c0af46',
+    padding: 10,
+    width: '100%',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  loginButtonText: {
+    color: '#000000',
+    fontWeight: 'bold',
+  },
+});
 
 export default LoginScreen;
