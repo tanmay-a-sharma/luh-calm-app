5a369c1b7f60efcecd652eec72270967c83b233d
tanmay-a-sharma <40327232+tanmay-a-sharma@users.noreply.github.com>
2023-12-30 01:14:46 -0800

    LFG mongodb hooked and active and court models done

---
+++ api/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config({ path: '../.env' });
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+
+const app = express();
+const port = 8000;
+const cors = require("cors");
+app.use(cors());
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+console.log(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI,{
+      useNewUrlParser:true,
+      useUnifiedTopology: true,
+}).then(() => {
+      console.log("Connected to MongoDB")
+}).catch((err) => {
+      console.log("Error connected to MongoDB", err)
+
+})
+
+app.listen(port, ()=> {
+      console.log("Server is running on port 8000");
+})
