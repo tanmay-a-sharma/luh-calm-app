7f463c4e1b7fbb5cbdea81e8bc93b6f3b5a5f1da
tanmay-a-sharma <40327232+tanmay-a-sharma@users.noreply.github.com>
2024-01-06 02:59:34 -0500

    create a new screen for the password forgetters

---
+++ app/ForgotPassword/PasswordResetScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PasswordResetScreen = () => {
+  return (
+    <View>
+      <Text>PasswordResetScreen</Text>
+    </View>
+  )
+}
+
+export default PasswordResetScreen
+
+const styles = StyleSheet.create({})
