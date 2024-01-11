4df311e7fc94a051552eb2fee1cd876c8f14d2e6
tanmay-a-sharma <40327232+tanmay-a-sharma@users.noreply.github.com>
2024-01-11 16:57:44 -0500

    adding pledge class to schema, and refactoring

--- models/activeModel.js
+++ models/activeModel.js
@@ -1,65 +1,67 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 // all the user-required fields
-const activeSchema = new mongoose.Schema({
+const activeSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: [true, 'Please add a name']
+      type: String,
+      required: [true, "Please add a name"],
     },
     email: {
-        type: String,
-        required: [true, 'Please add an email'],
-        unique: true
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
     },
     password: {
-        type:String,
-        required: [true, 'Please add a password']
+      type: String,
+      required: [true, "Please add a password"],
     },
-    status:{
-        type:String,
-        required: [true, 'Please indicate AKPsi status']
+    pledgeClass: String,
+    status: {
+      type: String,
+      required: [true, "Please indicate AKPsi status"],
     },
-    verified:{
-        type:Boolean,
-        default: false,
+    verified: {
+      type: Boolean,
+      default: false,
     },
-    verificationToken:String,
-    profileImage:String,
-    userDescription:{
-        type:String,
-        default:null
+    verificationToken: String,
+    profileImage: String,
+    userDescription: {
+      type: String,
+      default: null,
     },
-    connections:[
-        {
-            type:mongoose.Schema.Types.ObjectId,
-            ref: "activeSchema"
-        }
+    connections: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "activeSchema",
+      },
     ],
-    connectionRequests:[
-        {
-            type:mongoose.Schema.Types.ObjectId,
-            res:"activeSchema"
-        }
+    connectionRequests: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        res: "activeSchema",
+      },
     ],
-    createdAt:{
-        type:Date,
-        default: Date.now
+    createdAt: {
+      type: Date,
+      default: Date.now,
     },
     role: {
-        type: String,
-        required: [true, 'Please add a role']   // default role will be send using the customer controller
+      type: String,
+      required: [true, "Please add a role"], // default role will be send using the customer controller
     },
     // phoneNumber: {  // Test on fronted whether the input is in number or not.
     //     type: Number,
     //     required: [true, 'Please add a phone number']
     // }
-    
-    // I need to further add role, phone number here later
-},
-{
-    timestamps: true
-}
-)
 
-const Active = mongoose.model('Active', activeSchema);
+    // I need to further add role, phone number here later
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Active = mongoose.model("Active", activeSchema);
 module.exports = Active;
