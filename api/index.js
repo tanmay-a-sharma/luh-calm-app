6a0a78e64e1918ebb8f8522b71c175f2c28a3461
tanmay-a-sharma <40327232+tanmay-a-sharma@users.noreply.github.com>
2024-01-14 20:14:04 -0500

    successfully connected to the mongodatabase

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: '../.env' });
+require("dotenv").config({ path: "../.env" });
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -8,22 +8,156 @@ const nodemailer = require("nodemailer");
 const app = express();
 const port = 8000;
 const cors = require("cors");
+const crypto = require('crypto');
+
 app.use(cors());
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // console.log(process.env.MONGO_URI);
-mongoose.connect(process.env.MONGO_URI,{
-      useNewUrlParser:true,
-      useUnifiedTopology: true,
-}).then(() => {
-      console.log("Connected to MongoDB")
-}).catch((err) => {
-      console.log("Error connected to MongoDB", err)
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // serverSelectionTimeoutMS: 500, // Increase the timeout to 20 seconds for debugging purposes
 
-})
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
 
-app.listen(port, ()=> {
-      console.log("Server is running on port 8000");
-})
+  })
+  .catch((err) => {
+    console.log("Error connected to MongoDB", err);
+  });
+
+app.listen(port, () => {
+  console.log("Server is running on port 8000");
+});
+
+const User = require('../models/ActiveModel.js');
+// now we boutta connect to the backend to register the user
+app.post("/register", async (req, res) => {
+  try {
+    const { name, pledgeClass, email, password, confirmedPassword } = req.body;
+    
+    // checking for user of email log
+    console.log(`Checking for existing user with email: ${email}`);
+
+    // mongoose.model('Users', activeSchema);
+    // const User = require('../models/ActiveModel.js');
+
+    const db = mongoose.connection;
+
+    // const existingUser = await db.collection('test').find({});
+
+    // existingUser.returnKey
+    // if (existingUser) {
+    //   console.log("Email already registered");
+    //   console.log(existingUser); // This will log the properties of the existing user
+    //   return res.status(400).json({ message: "Email is already registered" });
+    // }
+    // see if the user is already registering with an email that already exists
+    // if (existingUser) {
+    //   console.log("Email already registered");
+    //   return res.status(400).json({ message: "Email is already registered" });
+    // }
+
+
+
+    if (password != confirmedPassword){
+      console.log("Your passwords are not the same");
+      return res.status(400).json({message: "Your passwords are not the same"});
+    }
+
+
+    verificationToken = crypto.randomBytes(20).toString("hex");
+
+    const newUser = new User({
+      name: name,
+      pledgeClass: pledgeClass,
+      email: email,
+      password: password,
+      verificationToken: verificationToken,
+    });
+    
+
+    //await db.createCollection("active");
+    db.collection("active").insertOne(newUser);
+    //await newUser.save();
+
+    // we are going to send a verification email to the assigned user
+    sendVerificationEmail(newUser.email, newUser.verificationToken);
+    res.status(200).json({
+      message: "Registration successful. Check your email for verification",
+    });
+  } catch (error) {
+    console.log("error registering users", error);
+    res.status(500).json({ message: "Registration Failed when posting" });
+  }
+});
+
+const sendVerificationEmail = async (email, verificationToken) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "tanmayxsharma4@gmail.com",
+      pass: process.env.PASS,
+    },
+  });
+  const mailOptions = {
+    from: "tanmayxsharma4@gmail.com",
+    to: email,
+    subject: "Email Verification",
+    text: `Please click the following link to verify your email: http://localhost:8000/verify/${verificationToken}`,
+
+  };
+
+  // send the email
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log("Verification Email sent succsessfully");
+  } catch (error) {
+    console.log(error);
+    console.log("Error sending the verification email");
+  }
+};
+
+// endpoint to verify the emai
+
+app.get("/verify/:token", async (req, res) => {
+  try {
+    const token = req.params.token;
+    const db = mongoose.connection;
+    const user = await db.collection("active").findOne({verificationToken:token});
+    // const user = await User.findOne({ verificationToken: token });
+    if (!user) {
+      return res.status(404).json({ message: "Invalid verification token" });
+    }
+
+    // mark the user as verified
+    user.verified = true;
+    db.collection("active").updateOne(
+      {
+        _id: user._id,
+      },
+      [
+        {$set: {verified:true, verificationToken:null}}
+        
+      ]
+    );
+
+
+
+    // remove token from backend
+    user.verificationToken = undefined;
+    
+
+
+
+    res.status(200).json({ message: "Email verified successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Email verification failed" });
+    console.log(error);
+  }
+});
--- app/Register/RegisterScreen.js
+++ app/Register/RegisterScreen.js
@@ -1,92 +1,162 @@
-import React, { useState } from 'react';
+import { Picker } from '@react-native-picker/picker';
+import axios from "axios";
+import React, { useState } from "react";
 import {
+  Alert,
   SafeAreaView,
+  ScrollView,
   StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
-  View
-} from 'react-native';
+  View,
+} from "react-native";
+
+
 
 function RegisterScreen({ navigation }) {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmedPassword, setConfirmedPassword] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmedPassword, setConfirmedPassword] = useState("");
+  const [pledgeClass, setPledgeClass] = useState("");
 
+  const handleRegister = () => {
+    const user = {
+      name: name,
+      pledgeClass: pledgeClass,
+      email: email,
+      password: password,
+      confirmedPassword: confirmedPassword,
+      
+    };
 
-  const handleRegister = async () => {
-    try {
-      const response = await fetch('https://your-backend-api.com/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ name, email, password, confirmPassword}),
+    axios
+      .post("http://localhost:8000/register", user)
+      .then((response) => {
+        console.log(response);
+        Alert.alert(
+          "Registration succesful:",
+          "You have been registered successfully"
+        );
+        setName("");
+        setEmail("");
+        setPassword("");
+        setConfirmedPassword("");
+        setPledgeClass("");
+      })
+      .catch((error) => {
+        Alert.alert(
+          "registration failed",
+          "an error occurred while registering"
+        );
+        console.log("registration failed on screen", error);
       });
-  
-      const data = await response.json();
-  
-      if (response.ok) {
-        // Handle successful login, e.g., navigate to another screen or store token
-        console.log('Login successful:', data);
-        // Example: navigation.navigate('ProfileScreen');
-      } else {
-        // Handle login failure
-        Alert.alert('Login Failed', data.message || 'Invalid credentials');
-      }
-    } catch (error) {
-      // Handle network errors
-      console.error('Login error:', error);
-      Alert.alert('Error', 'Unable to connect to the server');
-    }
   };
+
+  // const handleRegister = async () => {
+  //   try {
+  //     const response = await fetch('https://your-backend-api.com/login', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify({ name, email, password, confirmPassword, pledgeClass}),
+  //     });
+
+  //     const data = await response.json();
+
+  //     if (response.ok) {
+  //       // Handle successful login, e.g., navigate to another screen or store token
+  //       console.log('Login successful:', data);
+  //       // Example: navigation.navigate('ProfileScreen');
+  //     } else {
+  //       // Handle login failure
+  //       Alert.alert('Login Failed', data.message || 'Invalid credentials');
+  //     }
+  //   } catch (error) {
+  //     // Handle network errors
+  //     console.error('Login error:', error);
+  //     Alert.alert('Error', 'Unable to connect to the server');
+  //   }
+  // };
 
   // Function to handle the login logic will go here
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.loginContainer}>
-        <Text style={styles.loginTitle}>Create Your Profile</Text>
-        
-        <TextInput
-          style={styles.input}
-          placeholder="Full Name"
-          placeholderTextColor="#c0af46"
-          value={name}
-          onChangeText={setName}
-        />
 
-        <TextInput
-          style={styles.input}
-          placeholder="Email"
-          placeholderTextColor="#c0af46"
-          value={email}
-          onChangeText={setEmail}
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Password"
-          placeholderTextColor="#c0af46"
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Confirm Password"
-          placeholderTextColor="#c0af46"
-          value={confirmedPassword}
-          onChangeText={setConfirmedPassword}
-          secureTextEntry
-        />
-        <TouchableOpacity
-          style={styles.registerButton}
-          onPress={() => {/* Function to handle login */}}
-        >
-          <Text style={styles.registerButtonText}>Register</Text>
-        </TouchableOpacity>
-      </View>
+      <ScrollView
+        contentContainerStyle={styles.scrollViewContainer}
+        bounces={true} // Enable bouncing effect
+      >
+
+        <View style={styles.loginContainer}>
+          
+          <Text style={styles.loginTitle}>Register</Text>
+
+          <TextInput
+            style={styles.input}
+            placeholder="Full Name"
+            placeholderTextColor="#c0af46"
+            value={name}
+            onChangeText={setName}
+          />
+
+          <Picker
+            selectedValue={pledgeClass}
+            onValueChange={(itemValue) => setPledgeClass(itemValue)}
+            style={styles.picker}
+            itemStyle={styles.pickerItem}
+          >
+            <Picker.Item label="Select Pledge Class" value="" />
+            
+            <Picker.Item label="Spring 21" value="Spring21" />
+            <Picker.Item label="Fall 21" value="Fall21" />
+            <Picker.Item label="Spring 22" value="Spring22" />
+            <Picker.Item label="Fall 22" value="Fall22" />
+            <Picker.Item label="Spring 23" value="Spring23" />
+            <Picker.Item label="Fall 23" value="Fall23" />
+          </Picker>
+
+        
+
+          
+
+          <TextInput
+            style={styles.input}
+            placeholder="Email"
+            placeholderTextColor="#c0af46"
+            value={email}
+            onChangeText={setEmail}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            placeholderTextColor="#c0af46"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Confirm Password"
+            placeholderTextColor="#c0af46"
+            value={confirmedPassword}
+            onChangeText={setConfirmedPassword}
+            secureTextEntry
+          />
+          <TouchableOpacity
+            style={styles.registerButton}
+            onPress={() => {
+              handleRegister();
+            }}
+          >
+            <Text style={styles.registerButtonText}>Register</Text>
+          </TouchableOpacity>
+        </View>
+
+        </ScrollView>
     </SafeAreaView>
   );
 }
@@ -97,58 +167,79 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    backgroundColor: '#093671',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#093671",
+    alignItems: "center",
+    justifyContent: "center",
   },
   loginContainer: {
-    width: '80%',
-    alignItems: 'center',
-    backgroundColor: 'white',
+    width: "95%",
+    alignItems: "center",
+    backgroundColor: "white",
     padding: 20,
     borderRadius: 8,
+    minWidth:300,
   },
   loginTitle: {
     fontSize: 22,
-    color: '#093671',
-    fontWeight: 'bold',
+    color: "#093671",
+    fontWeight: "bold",
     marginBottom: 20,
   },
-  input: {
+  picker: {
     width: '100%',
     backgroundColor: '#ecf0f1',
+    borderRadius: 5,
+    marginVertical: 8,
+    // For Android, you might be able to set the height directly:
+    height: 40, // Set the height to match your TextInput fields
+    // iOS-specific properties
+    justifyContent: 'center',
+  },
+  pickerItem: {
+    height: '100%',
+    color: 'black', // This is the text color inside the picker
+  },
+  input: {
+    width: "100%",
+    backgroundColor: "#ecf0f1",
     padding: 10,
     borderRadius: 5,
     marginVertical: 10,
   },
   loginButton: {
-    backgroundColor: '#c0af46',
+    backgroundColor: "#c0af46",
     padding: 10,
-    width: '100%',
-    alignItems: 'center',
+    width: "100%",
+    alignItems: "center",
     borderRadius: 5,
   },
   loginButtonText: {
-    color: '#000000',
-    fontWeight: 'bold',
+    color: "#000000",
+    fontWeight: "bold",
   },
   registerContainer: {
-    width: '80%', // Same width as loginContainer
-    alignItems: 'center', // Center the button
+    width: "80%", // Same width as loginContainer
+    alignItems: "center", // Center the button
     // Do not include background color or padding like in loginContainer
   },
   registerButton: {
     // Same style as loginButton
-    backgroundColor: '#c0af46',
+    backgroundColor: "#c0af46",
     padding: 10,
-    width: '100%', // Button takes full width of its container
-    alignItems: 'center',
+    width: "100%", // Button takes full width of its container
+    alignItems: "center",
     borderRadius: 5,
   },
   registerButtonText: {
     // Style for the register button text
-    color: '#000000',
-    fontWeight: 'bold',
+    color: "#000000",
+    fontWeight: "bold",
+  },
+  scrollViewContainer: {
+    flexGrow: 30, // Ensures that the content fills the space
+    justifyContent: 'center', // Centers content when it's smaller than screen
+    alignItems: 'center', // Aligns content horizontally
+    paddingHorizontal:10,
   },
 });
 
