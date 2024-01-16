18360af339e917fcad87126a67511d0a716762e6
tanmay-a-sharma <40327232+tanmay-a-sharma@users.noreply.github.com>
2024-01-15 23:58:08 -0500

    creating a new screen for the home page

---
+++ app/Landing/LandingScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+      SafeAreaView,
+      StyleSheet,
+      Text,
+      View,
+} from 'react-native';
+function LandingScreen({navigation}){
+
+      return (
+      <SafeAreaView>
+            <View>
+                  <Text>
+                        wassup
+                  </Text>
+            </View>
+
+      </SafeAreaView>
+      );
+}
+
+export default LandingScreen;
+
+const styles = StyleSheet.create({})
